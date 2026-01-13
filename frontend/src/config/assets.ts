a9aef6aead17d3b78cc4f0e66cd88d8f4d8431e2
contrasanta <srihari221122@gmail.com>
2026-01-13 17:31:14 +0530

    added icon correctly

--- frontend/src/config/assets.ts
+++ frontend/src/config/assets.ts
@@ -1,2 +1,5 @@
+import groupifyIcon from "@/assets/gropify-icon.png";
+
 export const SPLINE_SCENE_URL = "https://prod.spline.design/8xA208d9Oo0YlLZj/scene.splinecode";
-export const GROUPIFY_LOGO = "/src/assets/gropify-icon.png";
+export const GROUPIFY_LOGO = groupifyIcon;
+
